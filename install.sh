a48c86a19b6511297de51824d9e626f3eb4086da
Andy Muehlhausen <andymule@gmail.com>
2019-01-19 20:58:13 +0100

    install.sh touchup

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-sudo cp explorer /usr/local/bin/explorer
-sudo cp vscode /usr/local/bin/vscode
+chmod 555 explorer
+chmod 555 vscode
+cp explorer /usr/local/bin/explorer
+cp vscode /usr/local/bin/vscode
